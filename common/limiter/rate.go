ec701194bebeac2008182a5f4cffdf6e24bd7447
Kaisa <168642285+ECYCloud@users.noreply.github.com>
2025-10-22 16:34:07 +0800

    Update

--- common/limiter/rate.go
+++ common/limiter/rate.go
@@ -31,31 +31,3 @@ func (w *Writer) WriteMultiBuffer(mb buf.MultiBuffer) error {
 	w.limiter.WaitN(ctx, int(mb.Len()))
 	return w.writer.WriteMultiBuffer(mb)
 }
-
-// Reader wraps a buf.Reader and applies rate limiting on reads (downlink or uplink depending on context).
-type Reader struct {
-	reader  buf.Reader
-	limiter *rate.Limiter
-	r       io.Reader
-}
-
-func (l *Limiter) RateReader(reader buf.Reader, limiter *rate.Limiter) buf.Reader {
-	return &Reader{
-		reader:  reader,
-		limiter: limiter,
-	}
-}
-
-func (r *Reader) Close() error {
-	return common.Interrupt(r.reader)
-}
-
-func (r *Reader) ReadMultiBuffer() (buf.MultiBuffer, error) {
-	mb, err := r.reader.ReadMultiBuffer()
-	if mb != nil {
-		ctx := context.Background()
-		// apply limit proportional to buffer length
-		_ = r.limiter.WaitN(ctx, int(mb.Len()))
-	}
-	return mb, err
-}
